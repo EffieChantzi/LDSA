17ce1ad158ddeb0d5b030730ee57e9daa334444b
EffieChantzi <EffieChantzi@users.noreply.github.com>
2015-05-25 20:30:10 +0200

    Update initial_mapper05.py

--- initial_mapper05.py
+++ initial_mapper05.py
@@ -31,10 +31,12 @@ for a_name in list_names:
         total_dels = 0
         total_skipped = 0
         total_unaligned = 0
+        flag = 0
 
         for l in list_reads:
                 list_cigar = []
                 if abs(l.query_alignment_length) > 1000:
+                        flag = flag + 1
                         list_cigar = l.cigartuples
                         list_ref_pos = l.get_reference_positions()
                         
@@ -75,10 +77,10 @@ for a_name in list_names:
                                 total_dels = total_dels + dels
                                 total_skipped = total_skipped + skipped
                                 total_unaligned = total_unaligned + unaligned
-                                
-        info_split = a_name.split('.')
-        print "individual: %s, country: %s, date: %s" % (info_split[0], info_split[4], info_split[6])
-        print "TM: %d - TI: %d - TD: %d - TS: %d - TU: %d" % (total_matches, total_ins, total_dels, total_skipped, total_unaligned)
+        if flag > 0:                        
+                info_split = a_name.split('.')
+                print "individual: %s, country: %s, date: %s" % (info_split[0], info_split[4], info_split[6])
+                print "TM: %d - TI: %d - TD: %d - TS: %d - TU: %d" % (total_matches, total_ins, total_dels, total_skipped, total_unaligned)
         if os.path.isfile(a_name) and os.path.isfile(bai_file):
                 os.remove(a_name)
                 os.remove(bai_file)
