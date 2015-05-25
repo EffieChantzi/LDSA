0a61df24097a5e21adb12e2b875fb7cc3e569e6b
EffieChantzi <EffieChantzi@users.noreply.github.com>
2015-05-25 13:50:56 +0200

    Update initial_mapper04.py

--- initial_mapper04.py
+++ initial_mapper04.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python
 
+import os
 import sys
 import pysam
 import glob
 import time
+from subprocess import call
+
 
 start_time = time.time()
 
 
-filepath = "/home/ubuntu/pysam_streaming/bam_filenames.txt"
+filepath = "/home/ubuntu/pysam_streaming/test_stream/bam_filenames.txt"
 fileurl = open(filepath, "r")
 list_names = fileurl.read().splitlines()
 
@@ -16,7 +19,9 @@ list_names = fileurl.read().splitlines()
 for a_name in list_names:
         #print a_name
         print a_name
-
+        bai_file = a_name + ".bai" 
+        call(['swift', 'download', 'GenomeData', a_name])
+        call(['swift', 'download', 'GenomeData', bai_file])
         #os.chdir("/home/ubuntu/pysam_streaming")
         samfile = pysam.AlignmentFile(a_name, "rb")
         list_reads = list(samfile.fetch(until_eof = True))
@@ -37,5 +42,7 @@ for a_name in list_names:
                                         if new_list[i] == 0:
                                                 matches = matches + new_list[i + 1]
                                 print matches
+        os.remove(a_name)
+        os.remove(bai_file)
 
 print("--- %s seconds ---" % (time.time() - start_time))
