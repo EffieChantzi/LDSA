90ff92c094e10c31c1fd2a26e1541407238e1b80
EffieChantzi <EffieChantzi@users.noreply.github.com>
2015-05-27 14:58:11 +0200

    Update our_mapper.py

--- our_mapper.py
+++ our_mapper.py
@@ -51,14 +51,14 @@ for a_name in list_names:
                                                 
                                                 
                                 if seq_matches > 0:
-                                        print "%s\t%s\t%\t%s\t%s" % (info_split[0], info_split[4], "SNPs", snps)        
+                                        print "%s\t%s\t%s\t%s" % (info_split[0], info_split[4], "SNPs", snps)        
                                 if matches > 0:
-                                        print "%s\t%s\t%\t%s\t%s" % (info_split[0], info_split[4], "Matches", matches)        
+                                        print "%s\t%s\t%s\t%s" % (info_split[0], info_split[4], "Matches", matches)        
                                 if ins > 0:
-                                        print "%s\t%s\t%\t%s\t%s" % (info_split[0], info_split[4], "Insertions", ins)
+                                        print "%s\t%s\t%s\t%s" % (info_split[0], info_split[4], "Insertions", ins)
                                 if dels > 0:
-                                        print "%s\t%s\t%\t%s\t%s" % (info_split[0], info_split[4], "Deletions", dels)    
+                                        print "%s\t%s\t%s\t%s" % (info_split[0], info_split[4], "Deletions", dels)    
                                 if unaligned > 0:
-                                        print "%s\t%s\t%\t%s\t%s" % (info_split[0], info_split[4], "Soft_clips", unaligned)
+                                        print "%s\t%s\t%s\t%s" % (info_split[0], info_split[4], "Soft_clips", unaligned)
                                 if skipped > 0:
-                                        print "%s\t%s\t%\t%s\t%s" % (info_split[0], info_split[4], "Skipped_regions", skipped) 
+                                        print "%s\t%s\t%s\t%s" % (info_split[0], info_split[4], "Skipped_regions", skipped) 
