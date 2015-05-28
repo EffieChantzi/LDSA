967299c6629e5a61f9a70ba2e0b8cb75d0f5e04f
EffieChantzi <EffieChantzi@users.noreply.github.com>
2015-05-28 15:31:23 +0200

    Update download.py

--- download.py
+++ download.py
@@ -1,5 +1,28 @@
 #!/usr/bin/env python
 
-from subprocess import call
+import os
+import sys
 
-call(['swift', 'download', 'GenomeData', 'NA20911.chrom20.ILLUMINA.bwa.GIH.low_coverage.20120522.bam'])
+
+
+
+filepath = "bam_filenames"
+fileurl = open(filepath, "r")
+list_names = fileurl.read().splitlines()
+
+
+for a_name in list_names:
+
+        counter = 0
+        bai_file = a_name + ".bai"
+        cmd1 = "swift download GenomeData " + a_name
+        cmd2 = "swift download GenomeData " + bai_file
+
+        try:
+                bam_d = os.system(cmd1)
+        except IOError as error:
+                print "Download of bam failed"
+        try:
+                bai_d = os.system(cmd2)
+        except IOError as error:
+                print "Download of bai failed"
