d6f6b658c648c04a2c5f21adfea694f11fa810ef
pinac0099 <pinac0099@gmail.com>
2015-06-11 23:02:38 +0300

    Update and rename test_swift_python.py to initialFiles/test_swift_python.py

--- initialFiles/test_swift_python.py
+++ initialFiles/test_swift_python.py
@@ -3,16 +3,16 @@
 import swiftclient
 from swiftclient import service
 
-username = 'efch0787'
-password = 'wpfeP429::ec'
-tenant_name = 'g2015016'
-#auth_url = 'http://smog.uppmax.uu.se:5000/v2.0'
+username = ''
+password = ''
+tenant_name = ''
+#auth_url = ''
 
 conn = swiftclient.client.Connection(auth_version='2',
                                       user=username,
                                       key=password,
                                       tenant_name=tenant_name,
-                                      authurl='http://smog.uppmax.uu.se:5000/v2.0',
+                                      authurl='',
 )
 
 #(response, obj) = conn.get_object('GenomeData', 'NA20911.chrom20.ILLUMINA.bwa.GIH.low_coverage.20120522.bam')
